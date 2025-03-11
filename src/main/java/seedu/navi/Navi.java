5f4c74a7dbc4a7ec414517e67b43c3e2e3cc9b7d
kevinchangckc <kevinchang1225@gmail.com>
2025-03-11 15:26:34 +0800

    Added greetings for Navi

--- src/main/java/seedu/navi/Navi.java
+++ src/main/java/seedu/navi/Navi.java
@@ -7,11 +7,16 @@ public class Navi {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = "Navi";
-        System.out.println("Hello from: " + logo);
+        String logo = "\n" +
+                " _   _             _ \n" +
+                "| \\ | | __ ___   _(_)\n" +
+                "|  \\| |/ _` \\ \\ / / |\n" +
+                "| |\\  | (_| |\\ V /| |\n" +
+                "|_| \\_|\\__,_| \\_/ |_|";
+        System.out.println("Good day! I am: " + logo);
         System.out.println("What is your name?");
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        System.out.println("Aye " + in.nextLine() + "!");
     }
 }
