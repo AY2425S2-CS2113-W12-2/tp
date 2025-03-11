b57160762fa302213ba91e9614e863ab337efc93
kevinchangckc <kevinchang1225@gmail.com>
2025-03-11 23:27:23 +0800

    Added bye response

--- src/main/java/seedu/navi/Navi.java
+++ src/main/java/seedu/navi/Navi.java
@@ -1,5 +1,6 @@
 package seedu.navi;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Navi {
@@ -13,10 +14,62 @@ public class Navi {
                 "|  \\| |/ _` \\ \\ / / |\n" +
                 "| |\\  | (_| |\\ V /| |\n" +
                 "|_| \\_|\\__,_| \\_/ |_|";
-        System.out.println("Good day! I am: " + logo);
-        System.out.println("What is your name?");
+        System.out.println("G'day mate! I'm: " + logo);
+        System.out.println("What’s your name, legend?");
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Aye " + in.nextLine() + "!");
+        System.out.println("Aye, " + in.nextLine() + "! Stoked to have ya here!");
+
+        // Start command processing
+        Navi ui = new Navi();
+        Parser parser = new Parser(ui);
+
+        System.out.println("Alrighty, what can I do for ya?");
+        while (true) {
+            String input = in.nextLine();
+            parser.handleCommand(input);
+        }
+    }
+
+    public void showExitMessage() {
+        System.out.println("_________________________________________");
+        System.out.println("Ahh, you’re headin’ off? No worries!");
+        System.out.println("Keep on keepin’ on, and we’ll catch ya next time! 🦘✨");
+        System.out.println("_________________________________________");
+    }
+
+    public void showError(String message) {
+        System.out.println("⚠ Whoops! " + message);
+        System.out.println("That one’s a bit crook, but no dramas – give it another go!");
+    }
+}
+
+/**
+ * Custom exception class for chatbot errors.
+ */
+class NaviException extends Exception {
+    public NaviException(String message) {
+        super("⚠ Oi! " + message + " Let's get this sorted, mate.");
+    }
+}
+
+class Parser {
+    private final Navi ui;
+
+    public Parser(Navi ui) {
+        this.ui = ui;
+    }
+
+    public void handleCommand(String input) {
+        try {
+            if (input.equalsIgnoreCase("bye")) {
+                ui.showExitMessage(); // Use instance method via object
+                System.exit(0);
+            } else {
+                throw new NaviException("That’s not quite right, mate. Give it another shot!");
+            }
+        } catch (NaviException e) {
+            ui.showError(e.getMessage());
+        }
     }
 }
