199b67e8c2540daacc44b770ad305f2d96d6b874
brianchanshiyuan <e1155337@u.nus.edu>
2025-03-14 10:52:01 +0800

    Added Junit test case for ShowExitMessage

--- src/test/java/seedu/navi/NaviTest.java
+++ src/test/java/seedu/navi/NaviTest.java
@@ -1,10 +1,31 @@
 package seedu.navi;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
+
 class NaviTest {
+    @Test
+    void testShowExitMessage() {
+        Navi navi = new Navi();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        navi.showExitMessage();
+
+        String expectedOutput = "------------------------------------------------------------\n" +
+                "Ahh, you’re headin’ off? No worries!\n" +
+                "Keep on keepin’ on, and we’ll catch ya next time!\n" +
+                "------------------------------------------------------------\n";
+        assertEquals(expectedOutput, outContent.toString());
+
+        System.setOut(System.out); // Restore original System.out
+    }
+
     @Test
     public void sampleTest() {
         assertTrue(true);
