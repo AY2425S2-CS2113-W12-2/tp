c2742d293378e97d3323c46991b4c91a6e251282
brianchanshiyuan <e1155337@u.nus.edu>
2025-03-15 14:37:41 +0800

    added hostel.txt and otherbuilding.txt. added the code to read hostel.txt and otherbuilding.txt

--- src/main/java/seedu/navi/storage/Storage.java
+++ src/main/java/seedu/navi/storage/Storage.java
@@ -2,8 +2,10 @@ package seedu.navi.storage;
 
 import seedu.navi.canteenfinder.CanteenFinder;
 import seedu.navi.canteenfinder.landmark.Faculty;
+import seedu.navi.canteenfinder.landmark.OtherBuildings;
 import seedu.navi.canteenfinder.landmark.canteen.Canteen;
 import seedu.navi.canteenfinder.landmark.canteen.stall.Stall;
+import seedu.navi.canteenfinder.landmark.Hostel;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,6 +17,9 @@ import java.util.Scanner;
 public class Storage {
     private static final String FACULTY_FILE = "data/faculty.txt";
     private static final String CANTEEN_FILE = "data/canteen.txt";
+    //newly added
+    private static final String HOSTEL_FILE = "data/hostel.txt";
+    private static final String OTHERBUILDING_FILE = "data/otherbuilding.txt";
 
     private static void insertFacultyFromData(String facultyData, ArrayList<Faculty> faculties,
                                               Map<String, Canteen> canteenMap) {
@@ -65,6 +70,54 @@ public class Storage {
         }
     }
 
+    private static void insertHostelFromData(String hostelData, ArrayList<Hostel> hostels,
+                                             Map<String, Canteen> canteenMap) {
+        String[] parts = hostelData.split(": ");
+        String hostelName = parts[0];
+        Hostel hostel = new Hostel(hostelName);
+        hostels.add(hostel);
+        String[] canteenNames = parts[1].split("\\|");
+        for (String canteenName : canteenNames) {
+            Canteen canteen = canteenMap.get(canteenName.trim());
+            hostel.setNearestCanteens(canteen);
+        }
+    }
+
+    private static void processHostelFromFile(ArrayList<Hostel> hostels,
+                                              Map<String, Canteen> canteenMap)
+            throws FileNotFoundException {
+        File f = new File(HOSTEL_FILE);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String hostelData = s.nextLine();
+            insertHostelFromData(hostelData, hostels, canteenMap);
+        }
+    }
+
+    private static void insertOtherBuildingFromData(String otherBuildingData, ArrayList<OtherBuildings> otherBuildings,
+                                                    Map<String, Canteen> canteenMap) {
+        String[] parts = otherBuildingData.split(": ");
+        String otherBuildingName = parts[0];
+        OtherBuildings otherBuilding = new OtherBuildings(otherBuildingName);
+        otherBuildings.add(otherBuilding);
+        String[] canteenNames = parts[1].split("\\|");
+        for (String canteenName : canteenNames) {
+            Canteen canteen = canteenMap.get(canteenName.trim());
+            otherBuilding.setNearestCanteens(canteen);
+        }
+    }
+
+    private static void processOtherBuildingFromFile(ArrayList<OtherBuildings> otherBuildings,
+                                                     Map<String, Canteen> canteenMap)
+            throws FileNotFoundException {
+        File f = new File(OTHERBUILDING_FILE);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String otherBuildingData = s.nextLine();
+            insertOtherBuildingFromData(otherBuildingData, otherBuildings, canteenMap);
+        }
+    }
+
     public static void processDataFromFiles() {
         Map<String, Canteen> canteenMap = new HashMap<>();
         try {
@@ -72,12 +125,35 @@ public class Storage {
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
         }
+
         ArrayList<Faculty> faculties = new ArrayList<>();
+        Map<String, Faculty> facultyMap = new HashMap<>(); // added facultyMap
         try {
             processFacultyFromFile(faculties, canteenMap);
+            for (Faculty faculty : faculties) { // added loop
+                facultyMap.put(faculty.getName(), faculty);
+            }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
         }
+
+        ArrayList<Hostel> hostels = new ArrayList<>(); // Added hostel ArrayList
+        try {
+            processHostelFromFile(hostels, canteenMap); // Added processHostelFromFile call
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+
+        ArrayList<OtherBuildings> otherBuildings = new ArrayList<>(); // Added Place ArrayList
+        try {
+            processOtherBuildingFromFile(otherBuildings, canteenMap); // Added processOtherBuildingFromFile call
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+
         CanteenFinder.LANDMARKS.addAll(faculties);
+        CanteenFinder.LANDMARKS.addAll(hostels); //added building to landmarks.
+        CanteenFinder.LANDMARKS.addAll(otherBuildings);
     }
 }
+
