8830b0f5fb079916a2004e49f6b8245c835dd6cd
KeerthanaBanukumar <>
2025-03-18 22:32:14 +0800

    Fix Checkstyle violation by avoiding star import

--- src/test/java/seedu/navi/favorites/FavoritesTest.java
+++ src/test/java/seedu/navi/favorites/FavoritesTest.java
@@ -2,7 +2,8 @@ package seedu.navi.favorites;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
