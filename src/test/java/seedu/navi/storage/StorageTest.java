3778a1e0889ce42e912af24ca39ad0d97cf6f69d
brianchanshiyuan <e1155337@u.nus.edu>
2025-03-21 22:31:16 +0800

    Added new Junit tests to test if distance is read and stored properly.

--- src/test/java/seedu/navi/storage/StorageTest.java
+++ src/test/java/seedu/navi/storage/StorageTest.java
@@ -3,6 +3,10 @@ package seedu.navi.storage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.navi.canteenfinder.CanteenFinder;
+import seedu.navi.canteenfinder.landmark.Faculty;
+import seedu.navi.canteenfinder.landmark.Hostel;
+import seedu.navi.canteenfinder.landmark.Landmark;
+import seedu.navi.canteenfinder.landmark.OtherBuildings;
 import seedu.navi.canteenfinder.landmark.canteen.Canteen;
 import seedu.navi.canteenfinder.landmark.canteen.stall.Stall;
 import seedu.navi.canteenfinder.landmark.canteen.stall.StallCharacteristic;
@@ -13,11 +17,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Optional;
+
+import static seedu.navi.canteenfinder.CanteenFinder.LANDMARKS;
 
 public class StorageTest {
 
     private static final String TEST_DATA_DIR = "testdata/";
     private static final String CANTEEN_FILE = TEST_DATA_DIR + "canteen.txt";
+    private static final String HOSTEL_FILE = TEST_DATA_DIR + "hostel.txt";
+    private static final String FACULTY_FILE = TEST_DATA_DIR + "faculty.txt";
+    private static final String OTHER_BUILDING_FILE = TEST_DATA_DIR + "otherbuilding.txt";
 
     @BeforeEach
     public void setUp() throws IOException {
@@ -34,6 +44,25 @@ public class StorageTest {
                 Flavours | Mala | F | F | F | T
                 """;
         createFile(CANTEEN_FILE, canteenData);
+
+        String hostelData = """
+                Raffles: Techno Edge (550m) | Fine Food (650m)
+                Eusoff: The Deck (400m) | Flavours (1600m)
+                """;
+        createFile(HOSTEL_FILE, hostelData);
+
+        String facultyData = """
+                Science: Techno Edge (1000m) | The Deck (2000m)
+                Arts: Fine Food (3000m) | Flavours (4000m)
+                """;
+        createFile(FACULTY_FILE, facultyData);
+
+        String otherBuildingData = """
+                Library: Techno Edge (1500m) | Fine Food (2500m)
+                Museum: The Deck (3500m) | Flavours (4500m)
+                """;
+        createFile(OTHER_BUILDING_FILE, otherBuildingData);
+
     }
 
     private void createFile(String filePath, String content) throws IOException {
@@ -120,7 +149,62 @@ public class StorageTest {
         assert !malaChar.getVegetarian();
     }
 
+    @Test
+    public void testProcessDataFromFiles_hostelDistancesAreCorrect() {
+        Storage.processDataFromFiles();
+        Hostel raffles = findLandmarkByName("Raffles", Hostel.class);
+        assert raffles != null;
+        assert raffles.getCanteenDistance(Storage.getCanteenMap().get("Techno Edge")) == 550;
+        assert raffles.getCanteenDistance(Storage.getCanteenMap().get("Fine Food")) == 650;
+
+        Hostel eusoff = findLandmarkByName("Eusoff", Hostel.class);
+        assert eusoff != null;
+        assert eusoff.getCanteenDistance(Storage.getCanteenMap().get("The Deck")) == 400;
+        assert eusoff.getCanteenDistance(Storage.getCanteenMap().get("Flavours")) == 1600;
+    }
+
+    @Test
+    public void testProcessDataFromFiles_facultyDistancesAreCorrect() {
+        Storage.processDataFromFiles();
+        Faculty science = findLandmarkByName("Science", Faculty.class);
+        assert science != null;
+        assert science.getCanteenDistance(Storage.getCanteenMap().get("Techno Edge")) == 1100;
+        assert science.getCanteenDistance(Storage.getCanteenMap().get("The Deck")) == 1800;
+
+        Faculty arts = findLandmarkByName("FASS", Faculty.class);
+        assert arts != null;
+        assert arts.getCanteenDistance(Storage.getCanteenMap().get("Fine Food")) == 1400;
+        assert arts.getCanteenDistance(Storage.getCanteenMap().get("Flavours")) == 1600;
+    }
+
+    @Test
+    public void testProcessDataFromFiles_otherBuildingDistancesAreCorrect() {
+        Storage.processDataFromFiles();
+        OtherBuildings library = findLandmarkByName("CLB", OtherBuildings.class);
+        assert library != null;
+        assert library.getCanteenDistance(Storage.getCanteenMap().get("Techno Edge")) == 200;
+        assert library.getCanteenDistance(Storage.getCanteenMap().get("Fine Food")) == 1000;
+
+        OtherBuildings sportsHall = findLandmarkByName("MPSH", OtherBuildings.class);
+        assert sportsHall != null;
+        assert sportsHall.getCanteenDistance(Storage.getCanteenMap().get("The Deck")) == 1500;
+        assert sportsHall.getCanteenDistance(Storage.getCanteenMap().get("Flavours")) == 900;
+    }
+
+    private <T extends Landmark> T findLandmarkByName(String name, Class<T> landmarkClass) {
+        Optional<Landmark> landmark = LANDMARKS.stream()
+                .filter(l -> l.getName().equals(name))
+                .findFirst();
+
+        if (landmark.isPresent() && landmarkClass.isInstance(landmark.get())) {
+            return landmarkClass.cast(landmark.get());
+        } else {
+            return null;
+        }
+    }
+
     public static Map<String, Canteen> getCanteenMap() {
         return Storage.getCanteenMap();
     }
 }
+
