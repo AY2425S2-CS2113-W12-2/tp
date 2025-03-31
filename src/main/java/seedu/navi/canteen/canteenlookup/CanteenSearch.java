3dcd0667a8187cc5e0fc7c0c577a02b78265d1b4
brianchanshiyuan <e1155337@u.nus.edu>
2025-04-01 01:15:53 +0800

    Added canteen search function

--- src/main/java/seedu/navi/canteen/canteenlookup/CanteenSearch.java
+++ src/main/java/seedu/navi/canteen/canteenlookup/CanteenSearch.java
@@ -1,5 +1,41 @@
 package seedu.navi.canteen.canteenlookup;
 
+import seedu.navi.canteen.canteenfinder.landmark.canteen.Canteen;
+import seedu.navi.canteen.canteenfinder.landmark.canteen.stall.Stall;
+import seedu.navi.canteen.storage.CanteenDataProcessor;
+import seedu.navi.textui.TextUi;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CanteenSearch {
-    public static void searchCanteen(String canteenName) {}
+
+    public static void searchCanteen(String canteenName) {
+        // Create the canteenMap and dataProcessor
+        Map<String, Canteen> canteenMap = new HashMap<>();
+        CanteenDataProcessor dataProcessor = new CanteenDataProcessor(canteenMap);
+
+        try {
+            dataProcessor.processData();
+        } catch (FileNotFoundException e) {
+            TextUi.printCanteenDataFileNotFound();
+            return; // Exit if file is not found
+        }
+
+        Canteen canteen = canteenMap.get(canteenName);
+
+        if (canteen == null) {
+            System.out.println("Canteen not found: " + canteenName);
+            return;
+        }
+
+        System.out.println("Canteen Name: " + canteen.getName());
+        System.out.println("Stalls:");
+
+        ArrayList<Stall> stallList = new ArrayList<>(canteen.getStalls());
+
+        TextUi.printValidStallsCF(stallList);
+    }
 }
