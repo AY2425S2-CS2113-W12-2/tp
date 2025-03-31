0fb9ba78da615fd5419cb28fd2ad7b5408705a17
brianchanshiyuan <e1155337@u.nus.edu>
2025-03-31 20:48:40 +0800

    added .toLowerCase()

--- src/main/java/seedu/navi/canteen/canteenlookup/CanteenLookup.java
+++ src/main/java/seedu/navi/canteen/canteenlookup/CanteenLookup.java
@@ -9,13 +9,20 @@ public class CanteenLookup {
         TextUi.printCanteenLookupGreetingCL();
         String command;
         while (true) {
-            command = TextUi.IN.nextLine().trim();
+            command = TextUi.IN.nextLine().trim().toLowerCase();
             if (command.equalsIgnoreCase("e") ||
                     command.equalsIgnoreCase("exit")) {
                 TextUi.printExitCanteenLookupCL();
                 break;
             }
             String canteenName = CanteenShortcuts.CANTEEN_MAP.get(command);
+            if (canteenName == null) {
+                if (dataProcessor.getCanteenMap().containsKey(command)) {
+                    canteenName = command;
+                } else {
+                    canteenName = command;
+                }
+            }
             CanteenSearch.searchCanteen(canteenName, dataProcessor);
         }
     }
